b450e49a15fa1ca2a303b202009ae8e89013facc
horlahuk <olaitan.u.k@gmail.com>
2023-05-09 09:41:10 -0700

    add 12-fizzbuzz.py code

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0:
+            number = 'Fizz'
+        elif number % 5 == 0:
+            number = 'Buzz'
+        elif number % 3 == 0 and number % 5 == 0:
+            number = 'Fizzbuzz'
+
+        print('{:d}'.format(number), end=" ")

b7eb8cc1c216c92a917366863998b3ee51c26732
horlahuk <olaitan.u.k@gmail.com>
2023-05-10 05:34:14 -0700

    added 100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+output = ""
+for char in range(122, 96, -1):
+    if char % 2 == 0:
+        output += "{:c}".format(char)
+    else:
+        output += "{:c}".format(char - 32)
+
+print(output)

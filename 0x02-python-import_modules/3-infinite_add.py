9ad9526cf5a25b843ff39e364346458f0987a7e5
horlahuk <olaitan.u.k@gmail.com>
2023-05-11 14:07:38 -0700

    add 3-infinite_add.py file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv)
+
+    result = 0
+
+    for i in range(1, n):
+        result += int(sys.argv[i])
+    print(f"{result:d}")

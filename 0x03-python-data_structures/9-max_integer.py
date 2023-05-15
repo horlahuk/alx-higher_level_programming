d731d108adb0b23db13c043b1dd0b14a15fa9a18
horlahuk <olaitan.u.k@gmail.com>
2023-05-15 04:25:25 -0700

    add 9-max_integer.py file

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    
+    length = len(my_list)
+
+    if length == 0:
+        return None
+
+    my_list.sort()
+    return my_list[length - 1]

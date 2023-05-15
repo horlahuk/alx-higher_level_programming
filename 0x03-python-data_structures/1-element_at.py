3a8caf612d22e5458c112bb456b16071561f9836
horlahuk <olaitan.u.k@gmail.com>
2023-05-15 02:58:42 -0700

    add 1-element_at.py file

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    if idx < 0:
+        return None
+
+    length = len(my_list)
+
+    if idx >= length:
+        return None
+
+    for i in range(length):
+        if i == idx:
+            return my_list[i]
+    return None

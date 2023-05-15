453c6e9ec6b53d6e112a6275dc883c5184e42319
horlahuk <olaitan.u.k@gmail.com>
2023-05-15 03:09:31 -0700

    add 2-replace_in_list.py file

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    
+    if idx < 0:
+        return my_list
+
+    length = len(my_list)
+
+    if idx >= length:
+        return my_list
+
+    for i in range(length):
+        if i == idx:
+            my_list[i] = element
+    return my_list

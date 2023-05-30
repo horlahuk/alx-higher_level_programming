87aaf4e45a0e82578c4312a34fb1fac78336a867
horlahuk <olaitan.u.k@gmail.com>
2023-05-30 11:06:03 -0700

    add 3-square.py file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,7 +7,7 @@ class Square:
     """Instantiation witb size, raise TypeError or ValueError if size is not an int or value < 0"""
     def __init__(self, size=0):
         self.__size = size
-        if type(size) is not integer:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")

2d79b1167634445f4d9ec212752d26697ca1796e
horlahuk <olaitan.u.k@gmail.com>
2023-05-30 10:25:13 -0700

    add 2-square.py file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a square based on 1-square.py"""
+
+
+class Square:
+    """Instantiation will raise both TypeError and ValueError"""
+    def __init(self, size):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    

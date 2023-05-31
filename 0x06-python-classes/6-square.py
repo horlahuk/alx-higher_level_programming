047dc5c9c9eaf91a0d9f9c811f2ce7d9c4a3b444
horlahuk <olaitan.u.k@gmail.com>
2023-05-30 18:12:03 -0700

    add 6-square.py file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -24,7 +24,7 @@ class Square:
         """Set the current size of the square"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 

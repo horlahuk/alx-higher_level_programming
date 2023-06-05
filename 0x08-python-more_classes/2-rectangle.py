a784c54107d5bdd04b54b18df3ee73b090c20e10
horlahuk <olaitan.u.k@gmail.com>
2023-06-05 04:42:22 -0700

    added 2-rectangle.py files

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -50,4 +50,4 @@ class Rectangle:
         """Perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.___width + self.__height)
+        return 2 * (self.__width + self.__height)

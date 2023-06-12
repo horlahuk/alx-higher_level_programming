36ab7d87013d2598dc065506bb9e50f24feb7e99
horlahuk <olaitan.u.k@gmail.com>
2023-06-12 03:43:56 -0700

    add 2-is_same_class.py and README.md file

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,4 +2,7 @@
 
 def is_same_class(obj, a_class):
     """checks if obj is an instance of a_class"""
-    return type(obj) is a_class
+    if type(obj) is a_class:
+        return True
+    else:
+        return False

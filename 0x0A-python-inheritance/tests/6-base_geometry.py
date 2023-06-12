b9ddb703b5e2b64cb0b0c0166393975901fe81c9
horlahuk <olaitan.u.k@gmail.com>
2023-06-12 15:46:40 -0700

    added 7-base_geometry.py file

--- 0x0A-python-inheritance/tests/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""BaseGeometry class implementation"""
-
-
-class BaseGeometry:
-    """Public instance method: Area"""
-    def area(self):
-        """Raises an exception"""
-        raise Exception("area() is not implemented")

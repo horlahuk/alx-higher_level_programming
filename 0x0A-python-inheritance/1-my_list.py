ab4c78629a9fd8ce9ef55aca0d6be37da41efab4
horlahuk <olaitan.u.k@gmail.com>
2023-06-12 15:25:05 -0700

    added 1-my_list.py file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,7 +6,4 @@ class MyList(list):
     """Class MyList"""
     def print_sorted(self):
         """prints a sorted list of list"""
-        sorted_list = self[:]
-        sorted_list = sorted(sorted_list)
-        print(sorted_list)
-        return sorted_list
+        print(sorted(self))

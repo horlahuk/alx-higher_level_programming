bb301cf62aa8c3e846d772f34971b8dfc087278a
horlahuk <olaitan.u.k@gmail.com>
2023-06-12 04:41:36 -0700

    add 0-lookup.py file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""List of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """returns list of attributes and methods of an obj"""
+    print(dir(obj))

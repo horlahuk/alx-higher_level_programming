71155d2b400377d5d1c4bc40f32cb7c4f461a99f
horlahuk <olaitan.u.k@gmail.com>
2023-07-19 09:43:34 -0700

    add task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_usa and the table
 -- cities (in the database hbtn_0d_usa)
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,

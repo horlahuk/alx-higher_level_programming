8f8cdcf131d21f64f79ddf5a3f5e1f3d4a7c6c99
horlahuk <olaitan.u.k@gmail.com>
2023-08-14 14:57:24 -0700

    add task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,8 +4,8 @@ const args = process.argv.length;
 
 if (args === 2) {
   console.log('undefined is undefined');
-}else if (args === 3) {
+} else if (args === 3) {
   console.log(process.argv[2] + ' is undefined');
-}else if (args === 4) {
-  console.log(process.argv[2] + ' is ' + process.argv[3])
+} else if (args === 4) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }

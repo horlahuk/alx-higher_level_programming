2e9f6ed0e3005408e16b6e726e27bd8be239146e
horlahuk <olaitan.u.k@gmail.com>
2023-09-26 10:45:00 -0700

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,14 +2,12 @@
 
 const fs = require('fs');
 
-filePath = process.argv[2];
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (error, data) => {
-	if (error) {
-		console.error(error);
-		return;
-	}
-	else {
-		console.log(data);
-	}
-})
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(data);
+  }
+});
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf-8', (error) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+})
